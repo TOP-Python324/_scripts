9825e9469f23d14b57469647df9cdb42c95e87b1
Gennadiy <renardes@gmail.com>
2023-05-21 15:55:17 +0500

    пример использования функции print()

---
+++ base/print1.py
@@ -0,0 +1,14 @@
+a = 1
+# str(a)
+
+b = [1, 2, 3]
+# str(b)
+
+def c():
+    pass
+# str(c)
+
+print(a, b, c)
+print(a, b, c, sep='#', end='\n')
+print(a, b, c, sep=', ', end=' ')
+print(a, b, c, sep='\n', end=' == ')
