d804eba59dbdd98c0712c445e9e6e2f5cdece594
Gennadiy <renardes@gmail.com>
2023-05-27 16:08:36 +0500

    сравнительные операторы

---
+++ base/logic1.py
@@ -0,0 +1,22 @@
+print(f'{10 < 100 = }')
+print(f'{7 < 7 = }')
+print(f'{10 <= 20 = }')
+print(f'{15 <= 15 = }\n')
+
+print(f'{10 > 1 = }')
+print(f'{2 > 2 = }')
+print(f'{9 >= 15 = }')
+print(f'{22 >= 22 = }\n')
+
+print(f'{1 == 1 = }')
+print(f'{-5 == 5 = }\n')
+
+print(f'{1 != 1 = }')
+print(f'{-5 != 5 = }\n')
+
+
+a = 10 == 10
+print(a, type(a))
+print(f'{int(a) = }')
+print(f'{float(a) = }')
+print(f'{str(a) = }')
