db449daf5460b5c8827ae666c4e5fce911e9ca09
Gennadiy <renardes@gmail.com>
2023-06-03 17:48:34 +0500

    примеры использования условного цикла while

---
+++ base/while2.py
@@ -0,0 +1,8 @@
+
+while True:
+    inp = input('число: ')
+    # условие прерывания цикла
+    if not inp:
+        break
+    print(int(inp)**3)
+
---
+++ base/while3.py
@@ -0,0 +1,10 @@
+
+command = input(' команда > ')
+while command:
+    if command == 'quit':
+        break
+    else:
+        print(command*2)
+    command = input(' команда > ')
+    
+
