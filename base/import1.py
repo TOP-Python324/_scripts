5e3aaeccab6baf2b25d5d15e753dc7ddf70c6c37
Gennadiy <renardes@gmail.com>
2023-07-01 22:22:20 +0500

    порядок поиска файлов при импорте

---
+++ base/import1.py
@@ -0,0 +1,15 @@
+import sys
+
+sys.path.insert(0, r'D:\G-Doc\YandexDisk\Job\TOP Academy\Python web\224\scripts')
+
+for line in sys.path:
+    print(line)
+print()
+
+
+import fstrings
+
+print()
+
+import for1
+
